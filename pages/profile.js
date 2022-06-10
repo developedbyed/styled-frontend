a45365e1e51907af8c2c3300906323c5a7c720c4
developedbyed <ed.magician@gmail.com>
2022-06-10 17:55:54 +0100

    Adding the User Profile

---
+++ pages/profile.js
@@ -0,0 +1,64 @@
+import { useRouter } from "next/router";
+// Specify Stripe secret api key here
+const stripe = require("stripe")(
+  `${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`
+);
+import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
+import styled from "styled-components";
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(ctx) {
+    // access the user session
+    const session = getSession(ctx.req, ctx.res);
+    const stripeId = session.user[`${process.env.BASE_URL}/stripe_customer_id`];
+    const paymentIntents = await stripe.paymentIntents.list({
+      customer: stripeId,
+    });
+
+    return { props: { orders: paymentIntents.data } };
+  },
+});
+
+export default function Profile({ user, orders }) {
+  const route = useRouter();
+
+  return (
+    user && (
+      <div>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <div>
+          {orders.map((order) => (
+            <Order key={order.id}>
+              <div>
+                <h1>Order Number: {order.id}</h1>
+                <h2>{order.amount}</h2>
+              </div>
+              <div>
+                <h1>Receipt Email {order.receipt_email}</h1>
+              </div>
+            </Order>
+          ))}
+        </div>
+        <button onClick={() => route.push("/api/auth/logout")}>Log out</button>
+      </div>
+    )
+  );
+}
+
+const Order = styled.div`
+  background: white;
+  margin: 2rem 0rem;
+  padding: 3rem;
+  display: flex;
+  justify-content: space-between;
+  h1 {
+    font-size: 1rem;
+    color: var(--primary);
+    margin-bottom: 0.5rem;
+  }
+  h2 {
+    font-size: 1rem;
+    color: var(--secondary);
+  }
+`;
